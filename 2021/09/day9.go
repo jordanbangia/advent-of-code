2f2cacdad155b5897d3379dfe144ef126805ef80
Jordan <jordan.bangia@gmail.com>
2021-12-09 23:45:13 -0500

    Day 9 part 2

--- 2021/09/day9.go
+++ 2021/09/day9.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 
 	"github.com/jordanbangia/advent-of-code/goutil"
@@ -15,11 +16,10 @@ func main() {
 	}
 
 	_ = solve1(input)
-	// _ = solve2(input)
+	_ = solve2(input)
 }
 
-func solve1(input []string) error {
-
+func parseHeightMap(input []string) [][]int {
 	hMap := make([][]int, len(input))
 
 	for i, l := range input {
@@ -29,6 +29,11 @@ func solve1(input []string) error {
 		}
 		hMap[i] = row
 	}
+	return hMap
+}
+
+func solve1(input []string) error {
+	hMap := parseHeightMap(input)
 
 	m := len(hMap)
 	n := len(hMap[0])
@@ -69,4 +74,73 @@ func solve1(input []string) error {
 
 func solve2(input []string) error {
 	// to find a basin, we need to find
+	hMap := parseHeightMap(input)
+
+	m := len(hMap)
+	n := len(hMap[0])
+
+	// 0 is unvisited
+	// 1 is visited
+	// 9 is impassible
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if hMap[i][j] != 9 {
+				hMap[i][j] = 0
+			}
+		}
+	}
+
+	countBasin := func(i, j int) int {
+		q := [][]int{[]int{i, j}}
+
+		basinSize := 0
+		for len(q) > 0 {
+			e := q[0]
+			x, y := e[0], e[1]
+			q = q[1:]
+
+			if hMap[x][y] != 0 {
+				continue
+			}
+
+			basinSize += 1
+			hMap[x][y] = 1
+
+			if x > 0 {
+				q = append(q, []int{x - 1, y})
+			}
+			if x < m-1 {
+				q = append(q, []int{x + 1, y})
+			}
+			if y > 0 {
+				q = append(q, []int{x, y - 1})
+			}
+			if y < n-1 {
+				q = append(q, []int{x, y + 1})
+			}
+		}
+		return basinSize
+	}
+
+	basinSizes := []int{}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if hMap[i][j] == 0 {
+				basin := countBasin(i, j)
+				basinSizes = append(basinSizes, basin)
+			}
+		}
+	}
+
+	sort.Ints(basinSizes)
+	fmt.Println(basinSizes)
+
+	k := len(basinSizes)
+
+	largestBasins := basinSizes[k-1] * basinSizes[k-2] * basinSizes[k-3]
+
+	fmt.Printf("Solution 2: %d\n", largestBasins)
+	return nil
 }
